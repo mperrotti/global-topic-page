1b981d2d15644455b70577f156634137e657cbde
mperrotti <mperrotti573@gmail.com>
2015-11-29 14:28:48 -0500

    added some untracked files

---
+++ assets/js/src/globalTopic_data.js
@@ -0,0 +1,37 @@
+//
+// For more info on how this magic works:
+// https://github.com/meetup/gimme
+//
+
+var views = new ViewManager(function(){
+	gimme.apiKey = "7060231d422c3421e3c13406e606631"; // use your own api key!
+
+	var groupId = getParameterByName('mugID') || 1579989,
+			eventId = getParameterByName('mupID') || 223601492,
+			topic   = getParameterByName('topic') || 'hiking',
+			topicID = 638, //hiking
+			lat     = 52.5167,
+			lon     = 13.3833;
+
+	// ↓ ↓ ↓ ↓ Where we pick which data we want ↓ ↓ ↓ ↓
+	var shoppingList = [
+		{"gimme": "groups", "key":"localGroups", "data": {"lat": lat, "lon": lon, "topic": topic, "page": 6}, "children": [
+			{"gimme": "events", "data": {"page": 1}, "match": [ ["id", "group_id"] ] }
+		]},
+		{"gimme": "groups", "key":"globalGroups", "data": {"topic": topic, "page": 10, "zip": ""}},
+		{"gimme": "topic_categories", "key":"sugTopics", "data": {"lat": lat, "lon": lon, "radius": 50, "page": 10}},
+		{"gimme": "recommended_topics", "key":"relatedTopics", "data": {"other_topics": topicID, "page": 10}}
+	];
+	// ↑ ↑ ↑ ↑ Where we pick which data we want ↑ ↑ ↑ ↑
+
+	gimme.get(shoppingList, true).then(function(data){
+		$.extend(views.data, data);
+
+		// Now that the data is all ready, go ahead and start the router
+		window.addEventListener('hashchange', processHash);
+		processHash();
+
+		console.log(views.data);
+	});
+
+});
---
+++ assets/js/src/globalTopic_interaction.js
@@ -0,0 +1,18 @@
+//
+// Fake topic search
+//
+function topicFuzzySearch(event) {
+	if (event.original.keyCode === 13) {
+		console.log('show some topix');
+	}
+}
+
+//
+// Mapbox code
+//
+function loadMap() {
+	// L.mapbox.accessToken = 'pk.eyJ1IjoibXBlcnJvdHRpIiwiYSI6ImNpaGpyOWZpdDBuZ2p0cGx6bnFoZWpyc2MifQ.i1DaUuwQ2EdEGmGqQcuTyw';
+	// var map = L.mapbox.map('map', 'mapbox.streets').setView([52.5167, 13.3833], 9);
+	console.log('load map fires here');
+	//console.log(views._main);
+}
